5a4b673604e30b5df71f54e2404da744eb8b20d8
Serena Sorensen <serena.sorensen7@gmail.com>
2022-03-30 10:16:40 -0600

    another method

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -14,3 +14,10 @@ function hasUniqueChars(word){
 console.log(hasUniqueChars("Monday"));
 console.log(hasUniqueChars("Moonday"));
 console.log(hasUniqueChars("Bob"));
+
+function hasUniqueChars2(word){
+    return new Set(word).size === word.length;
+}
+console.log(hasUniqueChars2("Monday"));
+console.log(hasUniqueChars2("Moonday"));
+console.log(hasUniqueChars2("Bob"));
