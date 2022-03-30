2fd4e72fe4e4a18b4150fb50444b2614cdc27764
Serena Sorensen <serena.sorensen7@gmail.com>
2022-03-30 09:59:32 -0600

    working code

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -1,1 +1,16 @@
-// Write your code below
+function hasUniqueChars(word){
+    for(let i = 0; i < word.length; i++){
+        for(let j = 0; j < word.length; j++){
+            if(i !== j){
+                if(word[i] === word[j]){
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+console.log(hasUniqueChars("Monday"));
+console.log(hasUniqueChars("Moonday"));
+console.log(hasUniqueChars("Bob"));
